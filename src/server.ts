d278578bf741b42a4eb2d20834db875d3957efb7
devhoonse <devhoonse@gmail.com>
2022-09-08 23:50:43 +0900

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,68 @@
+import { ApolloServer } from "apollo-server";
+import "reflect-metadata";
+import { buildSchema, Field, ObjectType, Query, Resolver } from "type-graphql";
+
+
+@ObjectType()
+class Author {
+
+  @Field()
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+
+@ObjectType()
+class Book {
+
+  @Field()
+  title: string;
+
+  @Field(type => Author)
+  authors: Array<Author> = [];
+
+  @Field()
+  year: number;
+
+  @Field({ nullable: true})
+  subTitle?: string;
+
+  constructor(title: string, name: string, year: number, subTitle?: string) {
+    this.title = title;
+    this.year = year;
+    this.authors.push(new Author(name));
+    if (subTitle) this.subTitle = subTitle;
+  }
+}
+
+
+const books: Array<Book> = [
+  new Book('토지', '박경리', 1969),
+  new Book('무정', '이광수', 1917)
+];
+
+
+@Resolver()
+class BookResolver {
+
+  @Query(() => [Book])
+  async books(): Promise<Array<Book>> {
+    return books;
+  }
+}
+
+
+async function main() {
+  const schema = await buildSchema({
+    resolvers: [BookResolver]
+  });
+
+  const server = new ApolloServer({ schema });
+  const { url } = await server.listen();
+
+  console.log('Server is Running on : ', url);
+}
+main().then();
